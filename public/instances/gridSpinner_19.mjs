f4e047a50f838d4b8e0c3c6110662166b7fcb0ef
Chris Goad <cagoad@gmail.com>
2023-05-06 15:03:58 -0600

    misc

--- public/instances/gridSpinner_19.mjs
+++ public/instances/gridSpinner_19.mjs
@@ -15,7 +15,7 @@ rs.setName('gridSpinner_19');
 let topParams = {width:wd,height:wd,numRows:nr,numCols:nr,numConnections:400,framePadding:.1*wd,stepsPerMove:10,numStepss:24,numSteps:400,center:Point.mk(0,0),radius:wd/4,
                  cycles:1,frameStroke:'rgb(2,2,2)',frameStrokee:'white',frameStrokeWidth:1,saveAnimation:1,stepInterval:40,randomConnections:1,
            //      pauseAtt:[29,30,59,60],numConnections:100,numPhases:102,showThePath:1,showIntersections:1}
-                 pauseAtt:[29,30,59,60],numConnections:10,numPhases:100,showThePath:0,showIntersections:0,numSpokes:11}
+                 pauseAtt:[29,30,59,60],numConnections:18,numPhases:100,showThePath:0,showIntersections:0,numSpokes:11}
             //     pauseAtt:[29,30,59,60],numConnections:4,numPhases:80,showThePath:0,showIntersections:1,numSpokes:5}
 Object.assign(rs,topParams);
 
@@ -77,7 +77,7 @@ rs.connectIndices = function (params) {
     e1pi = pi-1;
    // e1pi = pi;
   }
-  let e1si =(e0si+Math.floor(Math.random()*3))%np;
+  let e1si =(e0si+6+Math.floor(Math.random()*0))%np;
   //debugger;
   console.log('e0si',e0si,'e1pi',e1pi,'e1si',e1si);
   return {end0ShapeIndex:e0si,end1PathIndex:e1pi,end1ShapeIndex:e1si};
@@ -90,18 +90,19 @@ rs.addMotions = function () {
     let {cells,numPhases,shapeConnector,numSpokes} = this;
   let radius = .25;
   let startAngle = 0;//.25*(2*Math.PI);
-  let center = Point.mk(.5,.5);
+  //let center = Point.mk(.5,.5);
+  let center = Point.mk(.5,.6);
   let numPoints = 20;
   let path0 = this.mkCircle({radius:0.4,numPoints,center});
   let path1 = this.mkCircle({radius:0.3,numPoints,center});
   let path2 = this.setPathLength([Point.mk(.2,.3),Point.mk(.8,.3)],numPoints);
-  let path3 = this.setPathLength([Point.mk(.2,.2),Point.mk(.8,.2)],numPoints);
+  let path3 = this.setPathLength([Point.mk(.2,.1),Point.mk(.8,.1)],numPoints);
   let ipath0 = this.interpolatePaths(path0,path2);
   let ipath1 = this.interpolatePaths(path1,path3);
- // let fpath0 = path0.concat(ipath0.concat(path2))
   let fpath0 = path0.concat(ipath0,path2,path2);
   //let fpath1 = path1.concat(ipath1.concat(path3))
   let fpath1 = path1.concat(ipath1,path3,path3)
+  //let fpath1 = path0.concat(path3,ipath1,path3,path3)
   this.thePath = fpath1;
   //t paths= [path0,path1,path2,path3];
   //let paths= [ipath0,ipath1];
