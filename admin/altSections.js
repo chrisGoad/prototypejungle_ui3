30be8d37b22a8069e1a77802514747af582f725a
Chris Goad <cagoad@gmail.com>
2023-09-03 10:10:31 -0600

    misc

--- admin/altSections.js
+++ admin/altSections.js
@@ -3,7 +3,7 @@
 module.exports = {
 sections: 
 
-[  
+[      [0,'bounce_0','generators',1,'bounce_0',{likes:0,posted:0,category:'drop'}], 
       [0,'drop_circles_25','instances',1,'drop_circles_25',{likes:0,posted:0,category:'drop'}], 
       [0,'drop_circles_23','instances',1,'drop_circles_23',{likes:0,posted:0,category:'drop'}], 
       [0,'drop_circles_22','generators',1,'drop_circles_22',{likes:0,posted:0,category:'drop'}], 
