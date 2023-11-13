3ce234b07c98f3b6d95560f72dc797672d051e67
Chris Goad <cagoad@gmail.com>
2023-11-13 10:23:01 -0700

    misc

--- public/generators/stripes_0.mjs
+++ public/generators/stripes_0.mjs
@@ -27,10 +27,14 @@ rs.displayRectangle = function (r) {
 
 rs.displayRectangles = function (ra) {
   let rshapes = [];
-  ra.forEach((r) => 
-    rshapes.push(this.displayRectangle(r))
-  );
-  return rshapes;
+  let stripe = [];
+  ra.forEach((r) => {
+    let rsh  = this.displayRectangle(r);
+    rshapes.push(rsh);
+    stripe.push({rectangle:r,shape:rsh});
+    
+  });
+  return stripe;
 }
 
  
@@ -42,28 +46,37 @@ rs.addStripeRects = function (rects,params) {
   let hsl = 0.5*stripeLength;
   let hsw = 0.5*sw;
   let hsi = 0.5*si;
-  const genIrect = (xORy) => {
+  const genIVrect = (xORy) => { // interval
     let crn = v?Point.mk(sp-hsw,xORy):Point.mk(xORy,sp-hsw);
     let ext = v?Point.mk(sw,si):Point.mk(si,sw);
-    return Rectangle.mk(crn,ext);
+    let r = Rectangle.mk(crn,ext);
+    r.isI =0;
+    r.isV = v;
+    return r;
   }
-   const genSrect = (x) => {
+   const genIRrect = (x) => { // intersection
     let crn = v?Point.mk(sp-hsw,xORy):Point.mk(xORy,sp-hsw);
     let ext = Point.mk(sw,sw);
-    return Rectangle.mk(crn,ext);
+    let r = Rectangle.mk(crn,ext);
+    r.isI = 1;
+    r.isV = v;
+    return r;
   }
   let xORy = -hsl;
   for (let i=0;i<ns;i++) {
-     let irect = genIrect(xORy);
+     let irect = genIVrect(xORy);
      xORy = xORy+si;
-     let srect = genSrect(xORy);
+     let srect = genIRrect(xORy);
      xORy = xORy+sw;
-     rects.push(irect);
-     tsr.push(irect);
+     if (i) {
+       rects.push(irect);
+       tsr.push(irect);
+     }
+       
      rects.push(srect);
      tsr.push(srect);
   }
-  let irect = genIrect(xORy);
+  let irect = genIVrect(xORy);
   rects.push(irect);
   tsr.push(irect);
   return tsr;
@@ -72,23 +85,111 @@ rs.addStripeRects = function (rects,params) {
 rs.genStripesRects = function (params) {
   let {stripeWidth:sw,stripeInterval:si,numStripes:ns,stripePos:sp} = params;
   let rects = [];
+  let hstripes = [];
+  let vstripes = [];
   let isp = -0.5*((ns-1)*si+ns*sw-sw);
   const genStripes = (vertical) => {
     params.vertical = vertical;
     let csp = isp;
     for (let i=0;i<ns;i++) {
       params.stripePos = csp;
-      this.addStripeRects(rects,params);
+      let sr = this.addStripeRects(rects,params);
+      if (vertical) {
+        vstripes.push(sr);
+      } else {
+        hstripes.push(sr);
+      }
       csp = csp+sw+si;
     }
   }
   genStripes(0);
   genStripes(1);
-  return rects;
+  return {vstripes,hstripes}
+}
+
+
+rs.genStripesRectsV = function (params) {
+  let {initialStripeWidth:isw,finalStripeWidth:fsw,initialStripeInterval:isi,numStripes:ns} = params;
+  let fsi = isi*(fsw/isw);
+  let rects = [];
+  let hstripes = [];
+  let vstripes = [];
+ // let isp = -0.5*((ns-1)*si+ns*sw-sw);
+  let isp = 0;
+  let sdelta = fsw-isw;
+  let idelta = fsi-isi;
+
+  const genStripes = (vertical) => {
+    debugger;
+    params.vertical = vertical;
+    let csp = isp;
+    for (let i=0;i<ns;i++) {
+      let fr = i/(ns-1);
+      let hfr = 1-fr;
+      let hfrs = hfr*hfr;
+      let frs = fr*fr;
+      let  sw = isw + fr * sdelta;
+      let  si = isi + fr * idelta;
+      let  hsw = isw + hfr * sdelta;
+      let  hsi = isi + hfr * idelta;
+      let hns = Math.floor(ns*(hsw/isw));
+      console.log('hns',hns,'hfr',hfr);
+      let iparams = vertical?{stripeWidth:sw,stripeInterval:si,stripePos:csp,numStripes:ns}:
+                             {stripeWidth:isw,stripeInterval:isi,stripePos:csp,numStripes:hns}
+      let sr = this.addStripeRects(rects,iparams);
+      if (vertical) {
+        vstripes.push(sr);
+      } else {
+        hstripes.push(sr);
+      }
+      csp = csp+sw+si;
+    }
+  }
+  genStripes(0);
+  genStripes(1);
+  return {vstripes,hstripes}
 }
      
+     
     
+rs.paintStripe = function (s,index,hcolor,vcolor,vertical) {
+  let {version} = this;
+  let ln = s.length;
+  let numints = 0;
+  for (let i=0;i<ln;i++) {
+    let r = s[i];
+    let {rectangle:rect,shape} = r;
+    if (rect.isI) {
+      if (rect.isV) {
+        shape.hide();
+      } else {
+      
+        //if (!((index+numints)%3)) {
+        let cnd = (version===0)?(!((index+numints)%3)):0;
+        if (cnd) {
+          shape.fill = hcolor;
+        } else { 
+          shape.fill = vcolor;
+        }
+      }
+      numints++;
+    } else {
+      shape.fill = (rect.isV)?vcolor:hcolor;
+    }
+    shape.update();
+  }
+}
 
+
+
+rs.paintStripes = function (s,hcolor,vcolor,vertical) {
+  let ln = s.length;
+  for (let i=0;i<ln;i++) {
+    this.paintStripe(s[i],i,hcolor,vcolor,vertical);
+  }
+}
+        
+    
 
 
     
--- public/instances/stripes_0.mjs
+++ public/instances/stripes_0.mjs
@@ -4,10 +4,10 @@ import {rs as polylinePP} from '/shape/polyline.mjs';
 import {rs as generatorP} from '/generators/stripes_0.mjs'
 let rs = generatorP.instantiate();
 
-rs.setName('stripes_1')
+rs.setName('stripes_0')
 let ht=50;
 
-let topParams = {width:ht,height:ht,framePadding:0.1*ht,frameStroke:'white',frameStrokeWidth:.2,numSteps:3*4*32};
+let topParams = {width:ht,height:ht,framePadding:0.1*ht,frameStroke:'white',frameStrokeWidth:.2,numSteps:3*4*32,version:0};
 	
 Object.assign(rs,topParams);
 
@@ -18,7 +18,7 @@ rs.initProtos = function () {
     let rectP = this.rectP = rectPP.instantiate();
   rectP.fill = 'white';
   rectP.stroke = 'red';
-  rectP['stroke-width'] = 0.05;
+  rectP['stroke-width'] = 0.0;
   let polylineP = this.polylineP = polylinePP.instantiate();
   polylineP.stroke = 'white';
   polylineP['stroke-width'] = .05;
@@ -27,18 +27,32 @@ rs.initProtos = function () {
 
 rs.initialize = function () {
    debugger;
-  let {timePerStep,stopTime,fills,height:ht,boxD,numParticles:numP,yc,maxifc} = this;
+  let {timePerStep,stopTime,fills,height:ht,boxD,numParticles:numP,yc,maxifc,version} = this;
   let hht = 0.5*ht;
   this.initProtos();
   this.addFrame();
   this.set('rectShapes',arrayShape.mk());
   let rect0 = Rectangle.mk(Point.mk(-2,-2),Point.mk(4,4));
   //let rs0 = this.displayRectangle(rect0);
-  let params = {stripeWidth:1,stripeInterval:2,numStripes:15,stripePos:20,vertical:1};
+  let params = (version===0)?{stripeWidth:1,stripeInterval:1,numStripes:19,vertical:1}:
+                             {initialStripeWidth:1,finalStripeWidth:4,initialStripeInterval:3,numStripes:13};
  // let rects = [];
-  let rects = this.genStripesRects(params);
+  let stripes = (version===0)? this.genStripesRects(params):this.genStripesRectsV(params);
+  let {vstripes:vs,hstripes:hs} = stripes;
+  let vstripes = this.vstripes = [];
+  let hstripes = this.hstripes = [];
+  hs.forEach((s) => hstripes.push(this.displayRectangles(s)));
+  vs.forEach((s) => vstripes.push(this.displayRectangles(s)));
+  let hcolor = (version==0)?'rgb(10,10,10)':'red';
+  let vcolor = (version==0)?'rgb(200,200,200)':'blue';
+  this.paintStripes(hstripes,hcolor,vcolor,0);
+  this.paintStripes(vstripes,hcolor,vcolor,1);
+
+ // this.paintStripes(hstripes,'rgb(10,10,10)','rgb(200,200,200)');
+ // this.paintStripes(vstripes,'rgb(100,100,100)','rgb(200,200,200)',1);
  // this.addStripeRects(rects,params);
-  this.displayRectangles(rects);
+ // this.displayRectangles(hstripes);
+  //this.displayRectangles(vstripes);
 }
 
 export {rs}
