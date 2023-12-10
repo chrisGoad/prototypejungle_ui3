8904665995a6268eeaeb6afe8f21b3f4a66be544
Chris Goad <cagoad@gmail.com>
2023-12-10 09:26:34 -0700

    misc

--- admin/altSections.js
+++ admin/altSections.js
@@ -4,6 +4,7 @@ module.exports = {
 sections: 
 
 [     
+      [0,'interpolator_0','generators',1,'Interpolator 0',{likes:0,posted:0,category:'drop'}], 
       [0,'triangle_1','generators',1,'Triangle 1',{likes:0,posted:0,category:'drop'}], 
       [0,'triangle_0','generators',1,'Triangle 0',{likes:0,posted:0,category:'drop'}], 
       [0,'interpolate_colors_9','instances',1,'Interpolate Colors 9',{likes:0,posted:0,category:'drop'}], 
--- public/generators/interpolator_0.mjs
+++ public/generators/interpolator_0.mjs
@@ -7,7 +7,7 @@ import {rs as addAnimationMethods} from '/mlib/animate0.mjs';
 
 let rs = basicP.instantiate();
 addAnimationMethods(rs);
-rs.setName('triangle_1');
+rs.setName('interpolator_0');
 
 
 let ht= 100;
@@ -188,116 +188,39 @@ rs.addRow =  function (j) {
   this.mkGon(qvs);
 }
 
-rs.orthVector = function (sg) {
-  let {end0,end1} = sg;
-  let vec = end1.difference(end0);
-  let ln = vec.length();
-  let nvec = vec.times(1/ln);
-  let {x,y} =nvec;
-  let nv = Point.mk(-y,x);
-  return nv.times(1);
-}
 
-rs.orthSeg = function (sg) {
-  let e0 = sg.end0;
-  let ov = this.orthVector(sg).times(10);
-  let oseg = LineSegment.mk(e0,e0.plus(ov));
-  return oseg;
-}
-
-rs.distFromSeg = function (p,sg) {
-  let ov = this.orthVector(sg);
-  let e0 = sg.end0;
-  let pv = p.difference(e0);
-  let d = ov.dotp(pv);
-  return d;
-}
-
-rs.angleIntersectLineSegment = function (p,a,sg1) { 
-  let {width:wd} = this;
-  let e1 = Point.mk(Math.cos(a)*wd,Math.sin(a)*wd).plus(p);
-  let sg0 = LineSegment.mk(p,e1);
- // this.addSeg(sg0);
-  let intsct = sg0.intersect(sg1);
-  return intsct;
-}
-
-rs.intersectSegs = function (p,a,sgs) {
-  let ln = sgs.length;
-  for (let i=0;i<ln;i++) {
-    let sg = sgs[i]
-    let intsct = this.angleIntersectLineSegment(p,a,sg);
-    if (intsct) {
-      return {which:i,intersection:intsct};
-    }
-  }
-}
-
-rs.paintIt= function (params) {
-  let {cornerColors:cc,angle:a,segs,center}=params;
-  let {bbase,sbase,gons} =this;
-  debugger;
-  let [c0,c1,c2,c3] = cc;
-  let a2r = Math.PI/180;
-  let tht = bbase*Math.sin(60*a2r);
-  let [lsA,lsB,lsC]  = segs;
-  let ii = this.interpolateArrays;
-  let i0 =  this.intersectSegs(center,a+30*a2r,segs);
-  let i1 =  this.intersectSegs(center,a+150*a2r,segs);
-  let i2 =  this.intersectSegs(center,a+270*a2r,segs);
-  const getColor = (i) => {
-    let {which:wh,intersection:intsct} = i;
-    let seg = segs[wh];
-    let {end0,end1} = seg;
-    let ce0 = cc[wh];
-    let ce1 = cc[(wh+1)%3];
-    let svec = intsct.difference(end0);
-    let sln = svec.length();
-    let fr = sln/bbase;
-    let ca = this.interpolateArrays(ce0,ce1,fr);
-    return ca;
-  }
-  let ca0 = getColor(i0);
-  let ca1 = getColor(i1);
-  let ca2 = getColor(i2);
+rs.interpolateParams = function () {
+   let {gons,vertices,vParams,applicator} = this;
   let lng = gons.length;
+  let vl = vertices.length;
   for (let i = 0;i<lng;i++) {
     let gon = gons[i];
     let pt = gon.center;
-    
-    let dA = tht-this.distFromSeg(pt,lsA);
-    let dB = tht-this.distFromSeg(pt,lsB);
-    let dC = tht-this.distFromSeg(pt,lsC);
-    let dCnt = 2*(pt.difference(center)).length();
-    console.log('pt',pt,'dA',dA,'dB',dB,'dC',dC,dCnt);
-    let fA = 1/dA;
-    let fB = 1/dB;
-    let fC = 1/dC;
-    let fCnt = 1/dCnt;
-    let sum = c3?fA+fB+fC+fCnt:fA+fB+fC;
-    let nfA = fA/sum;
-    let nfB = fB/sum;
-    let nfC = fC/sum;
-    let nfCnt = fCnt/sum;
-    console.log('NFA',nfA,'nfB',nfB,'nfC',nfC);
-    let wc0A = ca0.map((v) => nfA*v);
-    let wc1B = ca1.map((v) => nfB*v);
-    let wc2C = ca2.map((v) => nfC*v);
-    let wc3Cnt;
-    if (c3) {
-      wc3Cnt = c3.map((v) => nfCnt*v);
+    let ds = vertices.map((v) => pt.distance(v));
+   
+    let fcs = ds.map((v)=>1/v);//factors
+    let sum =0;
+    fcs.forEach((v) => {
+      sum=sum+v;
+    });
+    let nfcs = fcs.map( (v) => v/sum);//normalized factors
+    let wvps = [];// weighted parameters
+    for (let j=0;j<vl;j++) {
+      let vp = vParams[j];
+      let nfc = nfcs[j];
+      let wvp = vp.map((v)=>nfc*v);
+      wvps.push(wvp);
     }
-    
-    debugger;
-    let suma = c3?this.sumArrays([wc0A,wc1B,wc2C,wc3Cnt]):this.sumArrays([wc0A,wc1B,wc2C]);
-    let fill = this.arrayToRGB(suma);
-    gon.fill = fill;
-    gon.update();
-
+    let suma= this.sumArrays(wvps);  //sum the weights
+    applicator.call(this,gon,suma);
   }
 }  
   
-  
+rs.applicator = function (gon,colorArray) {
+  let rgb = this.arrayToRGB(colorArray);
+  gon.fill = rgb;
+  gon.update();
+}  
 rs.initialize = function () {
   debugger;
   this.initProtos();
@@ -310,14 +233,11 @@ rs.initialize = function () {
   let a2r = Math.PI/180;
   let tht = bbase*Math.sin(60*a2r);
   let trisegs = this.trisegs = this.mkTriangleSegs(bbase);
+  let vertices = this.vertices = trisegs.map((v)=>v.end0);
   let [lsA,lsB,lsC] =  trisegs;
    this.addSeg(lsA);
   this.addSeg(lsB);
   this.addSeg(lsC);
-  let triCenter = this.triCenter = this.triangleCenter(bbase);
-  let olsA  = this.orthSeg(lsA);
-  let olsB = this.orthSeg(lsB);
-  let olsC = this.orthSeg(lsC);
   let n = 200;
   n = 50;
   let pal = this.pal = this.pointsAlong(lsA,n);
@@ -328,26 +248,11 @@ rs.initialize = function () {
     this.addRow(i);
   }
  
-
-  let lng = gons.length;
-  debugger;
-  let c0 = this.randomColorArray(10,250,3);
-  let c1 = this.randomColorArray(10,250,3);
-  let c2 = this.randomColorArray(10,250,3);
-  let c3 = this.randomColorArray(10,250,3);
- 
-  c0 = [250,250,250];
-  c1 = [0,0,0];
-  c2 = [250,0,0];
-  c2 = [0,0,0];
-  /* c0 = [250,0,0];
-  c1 = [0,250,0];
-  c2 = [0,0,250];
-  c3 = [250,250,0];*/
-  let cornerColors = this.cornerColors = [c0,c1,c2];//c3];
-  let cangle = this.cangle = 0;
-  let pparams = {cornerColors,angle:cangle,segs:trisegs,center:triCenter};
-  this.paintIt(pparams);
+  let c0 = [250,250,250];
+  let c1 = [0,0,0];
+  let c2 = [250,0,0];
+  this.vParams = [c0,c1,c2];
+  this.interpolateParams();
 
 }
  
