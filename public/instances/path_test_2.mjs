fa802b77fa55cb4f1a68fc1be5fe019b18f513ac
Chris Goad <cagoad@gmail.com>
2024-02-06 06:28:51 -0700

    misc

--- public/instances/path_test_2.mjs
+++ public/instances/path_test_2.mjs
@@ -18,8 +18,8 @@ let ht=50;
 
 
 let topParams = {width:ht,height:ht,angleOffset:0*Math.PI/10,framePadding:-0.1*ht,frameStroke:'white',frameStrokeWidth:.2,
-timePerStep:1/(16*32),stopTime:1,recordingMotion:1,saveAnimation:1,distanceThreshold:6,
-    circleRadius:.2,nearestFadeFactor:20,shapesPerPath:60,speed:1,segsPerCircle:20,radius:.4*ht,numSlices:8,bendRadius:4};
+timePerStep:1/(16*32),stopTime:1,recordingMotion:1,saveAnimation:1,distanceThreshold:3,
+    circleRadius:.2,nearestFadeFactor:20,shapesPerPath:200,speed:1,segsPerCircle:20,radius:.4*ht,numSlices:8,bendRadius:1.5};
 
 Object.assign(rs,topParams);
 let subParams ={speed:10,shapesPerRing:2};
@@ -43,81 +43,81 @@ rs.initProtos = function () {
 
 
 rs.addBend = function (paths,params) {
-  let {startPoint:sp,straightLength:sln,radius,lastLength:lln} = params;
-  let ep0 = sp.plus(Point.mk(sln,0));
+  let {startPoint:sp,straightLength:sln,radius,lastLength:lln,rightFacing:rf} = params;
+  let ep0 = sp.plus(Point.mk(rf?sln:-sln,0));
   let path0 = this.mkUniformPath([sp,ep0]);
-  let params1 = {fromDir:'right',startPoint:ep0,radius,dirChange:'clockwise'};
+  let params1 = {fromDir:rf?'right':'left',startPoint:ep0,radius,dirChange:rf?'clockwise':'counterclockwise'};
   let path1 = this.uturnToPath(params1);
   let yc  = Point.mk(0,2*radius);
   let sp2 = ep0.plus(yc);
-  let ep2 = lln?sp2.plus(Point.mk(-lln,0)):sp2.plus(Point.mk(-sln,0));
+  let ep2 = lln?sp2.plus(Point.mk(rf?-lln:lln,0)):sp2.plus(Point.mk(rf?-sln:sln,0));
   let path2 = this.mkUniformPath([sp2,ep2]);
   paths.push(path0,path1,path2);
   if (lln) {
     return ep2;
   }
-  let params3 = {fromDir:'left',startPoint:ep2,radius,dirChange:'counterclockwise'};
+  let params3 = {fromDir:rf?'left':'right',startPoint:ep2,radius,dirChange:rf?'counterclockwise':'clockwise'};
   let path3 = this.uturnToPath(params3);
   paths.push(path3);
-  //paths.push(path2,path3);
   return ep2.plus(yc);
 }
 
+rs.addE = function (paths,params) {
+  let {rightFacing:rf,startPoint:sp,straightLength:sln,lastLength:lln,radius} = params;
+  let bparams = {startPoint:sp,radius,rightFacing:rf,straightLength:sln};
+  let bendend = this.addBend(paths,bparams);
+  bparams.startPoint = bendend;
+  for (let i=0;i<3;i++) {
+    bendend = this.addBend(paths,bparams);
+    bparams.startPoint = bendend;
+  }
+  bparams.lastLength =lln;
+  bendend = this.addBend(paths,bparams);
+  bparams = {fromDir:rf?'left':'right',startPoint:bendend,radius,dirChange:'clockwise',radius};
+  let bend0 = this.bendToPath(bparams);
+  paths.push(bend0);
+  bendend = bend0[1].value;
+  let returnEnd = Point.mk(bendend.x,sp.y+radius);
+  let returnPath = this.mkUniformPath([bendend,returnEnd]);
+  paths.push(returnPath);
+  bparams = {fromDir:'up',startPoint:returnEnd,dirChange:rf?'clockwise':'counterclockwise',radius};
+  let bend1 = this.bendToPath(bparams);
+  bendend  =bend1[1].value;
+  paths.push(bend1);
+  let closingPath = this.mkUniformPath([bendend,sp]);
+  paths.push(closingPath); 
+}
 rs.initialize = function () {
    debugger;
    this.initProtos();
    let {radius,lineP,bendRadius} = this;
   this.addFrame();
   this.set('shapes',arrayShape.mk());
-  this.numSteps = 10000;
+  this.numSteps = 100;
   let lines = this.set('lines',arrayShape.mk());
   let polylines = this.set('polylines',arrayShape.mk());
-/*  let path0 = this.mkUniformPath([Point.mk(0,0),Point.mk(10,0)]);
-  let params1 = {fromDir:'right',startPoint:Point.mk(10,0),radius:4,dirChange:'clockwise'};
-  let path1 = this.uturnToPath(params1);
-  let path2 = this.mkUniformPath([Point.mk(10,8),Point.mk(0,8)])
-  let params3 = {fromDir:'left',startPoint:Point.mk(0,8),radius:4,dirChange:'clockwise'};
-  let path3 = this.uturnToPath(params3);;
-  let path = this.concatPaths([path0,path1,path2,path3]);*/
-  let paths = [];
+  let paths0 = [];
+  let paths1 = [];
   let sp = Point.mk(-5,-10);
-  let bparams = {startPoint:sp,straightLength:10,radius:bendRadius};
-  let bendend = this.addBend(paths,bparams);
-  bparams.startPoint = bendend;
-  bparams.lastLength =8;
-  bendend = this.addBend(paths,bparams);
-  bparams = {fromDir:'left',startPoint:bendend,radius,dirChange:'clockwise',radius:bendRadius};
-  let bend0 = this.bendToPath(bparams);
-  paths.push(bend0);
-  bendend = bend0[1].value;
-  let returnEnd = Point.mk(bendend.x,sp.y+bendRadius);
-  let returnPath = this.mkUniformPath([bendend,returnEnd]);
-  paths.push(returnPath);
-  bparams = {fromDir:'up',startPoint:returnEnd,dirChange:'clockwise',radius:bendRadius};
-  let bend1 = this.bendToPath(bparams);
-  bendend  =bend1[1].value;
-  paths.push(bend1);
-  let closingPath = this.mkUniformPath([bendend,sp]);
-  paths.push(closingPath);
-  //paths.forEach((path) => this.show2dPath(path,30));
-  //return;
-  let path  = this.concatPaths(paths);
-  //this.show2dPath(path,30);
-  //return;
-  //this.show2dPath(path1,30);
-  //this.show2dPath(path,30);
+  this.addE(paths0,{startPoint:sp,straightLength:10,radius:bendRadius,lastLength:14,rightFacing:0});
+  this.addE(paths1,{startPoint:sp,straightLength:10,radius:bendRadius,lastLength:14,rightFacing:1});  
+  let path0  = this.concatPaths(paths0);
+  let path1  = this.concatPaths(paths1);
+
+ 
   
   this.speedFun = (j,i) => {
     let iodd = i%2;
     let jodd = j%2;
-    let sp0 = jodd?.5:.5;
+    let sp0 = jodd?1:1;
     return iodd?2*sp0:sp0;
   }
-  let activePaths = [];
-  let Apaths0 = this.buildApaths([path]);
-  Apaths0.forEach((ap) => {ap.connectMe = 1});
- 
-  activePaths = this.activePaths = Apaths0;
+  let activePaths = this.activePaths = [];
+  let Apaths0 = this.buildApaths([path0]);
+  Apaths0.forEach((ap) => {ap.connectMe = 1;activePaths.push(ap)});
+  let Apaths1 = this.buildApaths([path1]);
+  Apaths1.forEach((ap) => {ap.connectMe = 1;activePaths.push(ap)});
+  return;
   //activePaths = this.activePaths = [].concat(Apaths0,Apaths1,Apaths2,Apaths3);
   let av = this.allValuesToConnect();
   this.addLinesBetweenPositions(av,lineP);
@@ -131,7 +131,7 @@ rs.updateState = function () {
   console.log('ssf',ssf,'ct',ct);
   debugger;
   this.runActivePaths();
-  //return;
+  return;
   let aps = activePaths.filter((ap) => ap.connectMe);
    let mpnts = aps.map((ap) => ap.value);
    const fn = (line,dist) => {
