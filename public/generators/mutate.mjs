c7a4f9c0d082c1497715abe65682a6fd115d8d11
Chris Goad <cagoad@gmail.com>
2024-02-21 14:17:42 -0700

    misc

--- public/generators/mutate.mjs
+++ public/generators/mutate.mjs
@@ -1,13 +1,12 @@
 import {rs as linePP} from '/shape/line.mjs';
 import {rs as polylinePP} from '/shape/polyline.mjs';
+import {rs as addAnimationMethods} from '/mlib/animate0.mjs';
 
 import {rs as basicP} from '/generators/basics.mjs';
-import {rs as addWordleMethods} from '/mlib/wordle.mjs';
 
 let rs = basicP.instantiate();
+addAnimationMethods(rs);
 
-
-addWordleMethods(rs);
 
 rs.setName('mutate');
 let ht=50;
@@ -101,62 +100,139 @@ rs.configureLines = function (params) {
     let left = -d;
     let right = d;
     let e0,e1;
+    
     let ul = lines[index+0];
-    let ule0 = Point.mk(left,top);
-    let ule1 = Point.mk(left,-hsep);
-    ul.setEnds(ule0,ule1);
+    e0 = Point.mk(left,top);
+    e1 = Point.mk(left,-hsep);
+    ul.setEnds(e0,e1);
     ul.update();
+    
     let um = lines[index+1];
-    let ume0 = Point.mk(0,top);
-    let ume1 = Point.mk(0,-hsep);
-    um.setEnds(ume0,ume1);
+    e0 = Point.mk(0,top);
+    e1 = Point.mk(0,-hsep);
+    um.setEnds(e0,e1);
     um.update();
+    
     let ur = lines[index+2];
-    let ure0 = Point.mk(right,top);
-    let ure1 = Point.mk(right,-hsep);
-    ur.setEnds(ure0,ure1);
+    e0 = Point.mk(right,top);
+    e1 = Point.mk(right,-hsep);
+    ur.setEnds(e0,e1);
     ur.update();
     
     
     let ll = lines[index+3];
-    let lle0 = Point.mk(left,hsep);
-    let lle1 = Point.mk(left,bot);
-    ll.setEnds(lle0,lle1);
-    ul.update();
+    e0 = Point.mk(left,hsep);
+    e1 = Point.mk(left,bot);
+    ll.setEnds(e0,e1);
+    ll.update();
+    
     let lm = lines[index+4];
-    let lme0 = Point.mk(0,hsep);
-    let lme1 = Point.mk(0,bot);
-    lm.setEnds(lme0,lme1);
+    e0 = Point.mk(0,hsep);
+    e1 = Point.mk(0,bot);
+    lm.setEnds(e0,e1);
     lm.update();
+    
     let lr = lines[index+5];
-    let lre0 = Point.mk(right,hsep);
-    let lre1 = Point.mk(right,bot);
-    lr.setEnds(lre0,lre1);
+    e0 = Point.mk(right,hsep);
+    e1 = Point.mk(right,bot);
+    lr.setEnds(e0,e1);
     lr.update();
   }
 }
-  
-  
+
+rs.clines =  function (n0,n1,fr) {
+  let {paramsv,paramsh} = this;
+  let paramsvi = this.interpolate(paramsv[n0],paramsv[n1],fr);
+  let paramshi = this.interpolate(paramsh[n0],paramsh[n1],fr);
+  paramsvi.index=0;
+  paramshi.index=6;
+  this.configureLines(paramsvi);
+  this.configureLines(paramshi);
+  }
+
 rs.initialize = function () {
-  debugger;   
+  debugger; 
+  let numSteps = this.numSteps = 128;
+  this.pauseSteps = numSteps/2;
   this.initProtos();
   let lines = this.set('lines',arrayShape.mk());
   this.addLines(12);
-  let params0,params1;
-  const clines  = ()=>{
-    this.configureLines(params0);
-    this.configureLines(params1);
-  }
-  params0= {index:0,center:Point.mk(0,0),horizontal:0,lineLength:10,lineSep:2,lineDist:5};
-  params1= {index:6,center:Point.mk(0,0),horizontal:1,lineLength:10,lineSep:0,lineDist:12};
-  clines();
+  let paramsv = this.paramsv=[];
+  let paramsh = this.paramsh=[];
+  //0
+  paramsv.push({index:0,center:Point.mk(0,0),horizontal:0,lineLength:10,lineSep:2,lineDist:5});;
+  paramsh.push({index:6,center:Point.mk(0,0),horizontal:1,lineLength:10,lineSep:0,lineDist:12});
   
-  params0= {index:0,center:Point.mk(0,0),horizontal:0,lineLength:10,lineSep:0,lineDist:12};
-  params1= {index:6,center:Point.mk(0,0),horizontal:1,lineLength:10,lineSep:2,lineDist:6};
-  //clines();
+  paramsv.push({index:0,center:Point.mk(0,0),horizontal:0,lineLength:10,lineSep:0,lineDist:12});
+  paramsh.push({index:6,center:Point.mk(0,0),horizontal:1,lineLength:8,lineSep:4,lineDist:12});
+  
+  paramsv.push({index:0,center:Point.mk(0,0),horizontal:0,lineLength:10,lineSep:0,lineDist:12});
+  paramsh.push({index:6,center:Point.mk(0,0),horizontal:1,lineLength:8,lineSep:4,lineDist:6});
+  this.clines(0,1,0);
 
 }
 
+rs.updateState = function () {
+  let {numSteps,stepsSoFar:ssf,pauseSteps:ps} = this;
+  debugger;
+  console.log('ssf',ssf);
+  if (ssf === 10) {
+    //this.paused = 1;
+  }
+  let ups = numSteps-ps;
+  let hups = ups/2;
+  let hps = ps/2;
+  let iv0 = [0,hps];//pause
+  let iv1 = [hps,hps+hups]; 
+  let iv2 = [hps+hups,ps+hups];//pause
+  let iv3 = [ps+hups,ps+ups];
+  const inInterval = (v,iv) => {
+    return (iv[0]<v) && (v <= iv[1]);
+  }
+   const fractionThruInterval = (v,iv) => {
+    return (v-iv[0])/(iv[1]-iv[0]);
+  }
+  
+  
+  if (inInterval(ssf,iv0) || inInterval(ssf,iv2)) {
+    return;
+  }
+  if (inInterval(ssf,iv1)) {
+    let fr = fractionThruInterval(ssf,iv1);
+    console.log('iv1','fr',fr);
+    if (fr <0.5) {
+      this.clines(0,1,2*fr);
+    } else {
+      this.clines(1,2,2*(fr-0.5));
+    }    
+  }
+  if (inInterval(ssf,iv3)) {
+    let fr = fractionThruInterval(ssf,iv3);
+    if (fr <0.5) {
+      this.clines(2,1,2*fr);
+    } else {
+      this.clines(1,0,2*(fr-0.5));
+    }    
+   
+  }
+}
+/*
+  if  ((ssf>(hups+hps))&& (ssf < (ups + hps))) {
+    return;
+  }    
+  if (fr<0.25) {
+    
+    this.clines(0,1,fr*4);
+  } else if (fr<.5) {
+    this.clines(1,2,(fr-0.25)*4);
+  } else if (fr<0.75) {
+    this.clines(2,1,(fr-0.5)*4);
+  } else  {
+    this.clines(1,0,(fr-0.75)*4);
+  }
+}
+  
+*/
 
  
 export {rs};
