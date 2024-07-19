ab08ca2a0940dd8bd92e013252afbaf62c9efad3
Chris Goad <cagoad@gmail.com>
2024-07-19 10:01:07 -0600

    misc

--- public/generators/grid_fading_circles_2.mjs
+++ public/generators/grid_fading_circles_2.mjs
@@ -4,9 +4,12 @@ import {rs as addGridMethods} from '/mlib/grid.mjs';
 import {rs as linePP} from '/shape/line.mjs';
 import {rs as circlePP} from '/shape/circle.mjs';
 import {rs as rectPP} from '/shape/rectangle.mjs';
+import {rs as addAnimationMethods} from '/mlib/animate0.mjs';
+
 let rs = basicP.instantiate();
 
 addGridMethods(rs);
+addAnimationMethods(rs);
 
 rs.setName('grid_fading_circles_2');
 let mul = 1;
@@ -21,7 +24,8 @@ let nr = nc;
 let ht = wd;//Math.floor((nc/nr) * ht)-1;
 let colorParams = {redOb:{r:255,g:0,b:0},greenOb:{r:0,g:255,b:0},blueOb:{r:0,g:0,b:255},blackOb:{r:0,g:0,b:0},whiteOb:{r:255,g:255,b:255},
                   cyanOb:{r:0,g:255,b:255}};
-let topParams = {width:wd,height:ht,numRows:nr,numCols:nc,framePadding:0.2*wd,frameStroke:undefined,doNotDisplayParts:1,numSteps:30,numCircles:4}
+let topParams = {width:wd,height:ht,numRows:nr,numCols:nc,framePadding:0.2*wd,frameStroke:undefined,doNotDisplayParts:1,numSteps:30,
+                 numCircles:16,numSteps:20,saveAnimation:1}
 Object.assign(rs,topParams);
 Object.assign(rs,colorParams);
 
@@ -75,7 +79,7 @@ rs.addFadingCircles = function (y) {
 
 rs.placeFadingCircle  = function (cix,ciy,offset) {
    let {numCircles,numCols,numRows,width,height,circles} = this;
-   debugger;
+   //debugger;
    let idx =cix + ciy*numCircles;
    let crc = circles[idx];
    let cbcx  = numCols/numCircles; // x cells between circles 
@@ -138,7 +142,7 @@ rs.shapeGenerator = function (rvs,cell) {
   let fr = this.frToLL(x,y);
   let gv = Math.floor(255*fr);
   console.log('x',x,'y',y,'fr',fr);
-  if (x<1) {
+  if (0) {
     fill = 'pink';
   } else {
     fill = this.toRGB(gv,gv,gv);
@@ -147,6 +151,44 @@ rs.shapeGenerator = function (rvs,cell) {
   return shape;
 }
 
+rs.setFills = function (fra) {
+  let {numCircles,circles,numCols,numRows,gv} = this;
+ // debugger;
+ // let fra = numCircles*ifra;
+  for (let j = 0;j<numCircles;j++) {
+    for (let i = 0;i<numCircles;i++) {
+      let idx = i+numCircles*j;
+      let crc = circles[idx];
+      let cbcx  = numCols/numCircles; // x cells between circles 
+      let cbcy  = numRows/numCircles; // y cells between circles 
+      let gx = cbcx*i;
+      let gy = cbcy*j;
+      let fr = this.frToLL(gx+fra*cbcx,gy);
+      let ofr = this.frToLL(gx,gy);
+     // let ogv = Math.floor(255*(1-fr))
+      console.log('gx',gx,'gy',gy,'fr',fr);
+      if (1&&(gx===0)) {
+        let sfr = this.frToLL(gx,gy);
+        let ofr2 = this.frToLL(gx+cbcx,gy);
+        let efr =1-ofr2; 
+        gv = 255*(sfr + fra * (efr-sfr));
+        debugger;
+      } else if (1&&(gx === (numCircles-1)*cbcx)) {
+        let sfr = 1-this.frToLL(gx,gy);
+        let ofr2 = this.frToLL(gx+cbcx,gy);
+        let efr =ofr2; 
+        gv = 255*(sfr + fra * (efr-sfr));
+        debugger;
+      } else {
+        gv = Math.floor(255*(1-fr));
+      }
+      let fill = this.toRGB(gv,gv,gv);
+      crc.fill=fill;
+      crc.update();
+    }
+  }
+}  
+
 rs.initialize =function ()  {
   debugger;
   this.initProtos();
@@ -154,15 +196,26 @@ rs.initialize =function ()  {
   let {width:wd,height:ht,numRows,numCols,numCircles} = this;
   let hwd = wd/2;
   let hht = ht/2;
+  let rectL = Rectangle.mk(Point.mk(-hwd-30,-hht),Point.mk(-hwd,hht));
   this.generateGrid();
   this.set('circles',core.ArrayNode.mk());
   for (let i=0;i<numCircles;i++) {
     let y = Math.floor((1/numCircles)*i*numRows);
     this.addFadingCircles(y);
   }
- this.placeFadingCircles(0.05);
+ // this.set('rectL',rectL);
+ //this.placeFadingCircles(0.05);
 }
 
+
+rs.updateState = function  () {
+  let {numSteps,stepsSoFar:ssf} = this;
+  debugger;
+  let fr = ssf/numSteps;
+  this.placeFadingCircles(fr);
+  this.setFills(fr);
+}
+ 
 
 export {rs};
 
