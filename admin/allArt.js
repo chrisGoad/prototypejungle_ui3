53d1131d5235c59971982fc5d501c789bb2c8549
Chris Goad <cagoad@gmail.com>
2024-08-02 19:08:05 -0600

    misc

--- admin/allArt.js
+++ admin/allArt.js
@@ -1,3 +1,8 @@
 module.exports = { sections:[ 
-[0,'art_hunters_in_the_snow','generators','','',{}], 
+[0,'art_hunters_in_the_snow','generators',1,'Hunters in the Snow',{}], 
+[0,'art_the_ambassadors','generators',1,'The Ambassadors',{}], 
+[0,'art_odds_and_ends','generators',1,'Odds and Ends',{}], 
+[0,'art_moonlight','generators',1,'Moonlight',{}], 
+[0,'art_christ_on_the_sea_of_galilee','generators',1,'Christ on the Sea of Galilee',{}], 
+[0,'art_saint_francis_in_the_desert','generators',1,'Saint Francis in the Desert',{}], 
 ]}
